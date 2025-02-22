da490e83baa6feaf7cdefecca65be02fa30cd69a
Dorji Gyem <02240117.cst@rub.edu.bt>
2025-02-22 20:49:29 +0530

    First commit

---
+++ inbuilt.py
@@ -0,0 +1,8 @@
+int('2.33')
+
+abs('-23.465')
+
+pow('3,4')
+
+age = input("Please enter your age")
+print(age)
--- onetxt.py
+++ onetxt.py
@@ -1,1 +1,6 @@
-hey, hello
+running_distance = 4*2
+print(running_distance)
+
+running_distance = running_distance + 3
+print(running_distance)
+
